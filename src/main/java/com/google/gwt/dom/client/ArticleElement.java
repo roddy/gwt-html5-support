444b1503c9b2237d30270bd03cd4802205054cd2
Roddy <roddyofthefrozenpeas@gmail.com>
2015-02-02 21:07:28 -0700

    Javadoc update
    Adding missing javadoc comments to article element

--- src/main/java/com/google/gwt/dom/client/ArticleElement.java
+++ src/main/java/com/google/gwt/dom/client/ArticleElement.java
@@ -2,15 +2,29 @@ package com.google.gwt.dom.client;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
+/**
+ * HTML5 article element.
+ *
+ * @see <a href="http://www.w3.org/html/wg/drafts/html/master/sections.html#the-article-element">W3C HTML5 Draft</a>
+ */
 @TagName(ArticleElement.TAG)
 public class ArticleElement extends Element {
     public static final String TAG = "article";
 
+    /**
+     * Assert that the given {@link Element} is compatible with this class and automatically typecast it.
+     */
     public static ArticleElement as(Element element) {
         assert is(element);
         return (ArticleElement) element;
     }
 
+    /**
+     * Determines whether the given {@link JavaScriptObject} can be cast to this class. A <code>null</code> object will
+     * cause this method to return <code>false</code>.
+     * @param o the object to test
+     * @return true if the given object can be cast to this class, false if otherwise or null
+     */
     public static boolean is(JavaScriptObject o) {
         if(Element.is(o)) {
             return is((Element)o);
@@ -18,6 +32,12 @@ public class ArticleElement extends Element {
         return false;
     }
 
+    /**
+     * Determines whether the given {@link Node} can be cast to this class. A <code>null</code> object will cause this
+     * method to return <code>null</code>.
+     * @param node the Node object to test
+     * @return true if the given Node can be cast to this class, false if otherwise or null
+     */
     public static boolean is(Node node) {
         if(Element.is(node)) {
             return is((Element)node);
@@ -25,12 +45,18 @@ public class ArticleElement extends Element {
         return false;
     }
 
-    public static boolean is(Element element) {
-        return element != null && element.hasTagName(TAG);
+    /**
+     * Determines whether the given {@link Element} can be cast to this class. A <code>null</code> object will cause
+     * this method to return <code>null</code>.
+     * @param elem the Element object to test
+     * @return true if the given Element can be cast to this class, false if otherwise or null
+     */
+    public static boolean is(Element elem) {
+        return elem != null && elem.hasTagName(TAG);
     }
 
     protected ArticleElement() {
-        
+
     }
 }
 
