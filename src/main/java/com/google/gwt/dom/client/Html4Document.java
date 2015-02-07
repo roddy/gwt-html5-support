8b5ab0fe7fc77ca2272a99ad128c0f5b42ef7e58
Roddy <roddyofthefrozenpeas@gmail.com>
2015-02-06 21:16:31 -0700

    Support for small tag
    Add create method to the Html4Document to support the creation of the
    small element.

--- src/main/java/com/google/gwt/dom/client/Html4Document.java
+++ src/main/java/com/google/gwt/dom/client/Html4Document.java
@@ -3,6 +3,14 @@ package com.google.gwt.dom.client;
 public class Html4Document extends Document {
 
     /**
+     * Creates a &lt;small&gt; element.
+     * @return the newly created element
+     */
+    public final SmallElement createSmallElement() {
+        return (SmallElement) DOMImpl.impl.createElement(this, SmallElement.TAG);
+    }
+    
+    /**
      * Creates a &lt;abbr&gt; element.
      * @return the newly created element
      */
